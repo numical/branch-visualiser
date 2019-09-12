b49c070b08b36aa97377dc39e13db700803de6a5
Mike Evans <mike.evans@numical.com>
2019-09-12 09:58:19 +0100

    start form building

--- src/client/actions/addBranch.js
+++ src/client/actions/addBranch.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 function addBranch(repo) {
-  return <pre>{JSON.stringify(repo, null, 2)}</pre>;
+  const commonProps = {};
+  const nameProps = {
+    ...commonProps
+  };
+  const descriptionProps = {
+    ...commonProps
+  };
+  const startProps = {
+    ...commonProps
+  };
+  const teamProps = {
+    ...commonProps
+  };
+  const fromProps = {
+    ...commonProps
+  };
+
+  return (
+    <form>
+      <label>Name:</label>
+      <input {...nameProps} />
+      <label>Description:</label>
+      <input {...descriptionProps} />
+      <label>Start:</label>
+      <input {...startProps} />
+      <label>Team:</label>
+      <input {...teamProps} />
+      <label>From:</label>
+      <input {...fromProps} />
+    </form>
+  );
 }
 
 addBranch.display = "Add Branch ...";
